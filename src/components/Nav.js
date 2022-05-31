e139404945f8bf5b4c4a1b85ecf2cb761a725b85
antikkorps <franckvienot7@gmail.com>
2022-06-01 00:21:11 +0200

    start styling and tweaking the navbar of the site

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,39 @@ import './Nav.scss';
 
 function nav() {
   return (
-    <div className='Nav'>
-      <button className='nav_burger'></button>
+    <div className='nav nav--black'>
+      <button className='nav_burger'>===</button>
       <img src='./images/logo.png' alt='logo' className='nav_logo' />
+      <nav className='nav__links'>
+        <a href='/' className='nav__link'>
+          Home
+        </a>
+        <a href='/' className='nav__link'>
+          Movies
+        </a>
+        <a href='/' className='nav__link'>
+          Tv-Shows
+        </a>
+      </nav>
+      <div className='nav__action'>
+        <a href='/' className='nav__action'>
+          Search
+        </a>
+        <a href='/' className='nav__action'>
+          Direct
+        </a>
+        <a href='/' className='nav__action'>
+          Gift
+        </a>
+        <a href='/' className='nav__action'>
+          Notification
+        </a>
+        <img
+          src='./images/pngtree-avatar.jpg'
+          alt='avatar'
+          className='avatar'
+        />
+      </div>
     </div>
   );
 }
